d9c4b16b5f647a2b6915e0cee631f4139e6f6529
PumpkinBall <s410385015@gmail.com>
2018-11-20 01:23:35 +0800

    ML Hw4 done

--- ML/Hw4/Hw4b.py
+++ ML/Hw4/Hw4b.py
@@ -1,5 +1,6 @@
 import numpy as np
 import os
+from PIL import Image
 from math import *
 from struct import *
 
@@ -46,6 +47,8 @@ def main():
     print(np.shape(label_train))
 
 
+   
+
     #Binarize the img
     image_train[image_train<=127]=0
     image_train[image_train>127]=1
@@ -54,61 +57,109 @@ def main():
     img_num,dim=np.shape(image_train)
     class_num=10
 
+    count=np.zeros(class_num)
+    for i in range(label_train.size):
+        count[label_train[i]]+=1
+
     _lambda=np.random.dirichlet(np.ones(10))
+    #_lambda=np.ones(10)/10
     pre_lambda=_lambda
     p=np.random.rand(class_num,dim)
     p=p/2+0.25
     w=np.empty([img_num,class_num])
     
-    print("lambda")
-    print(_lambda)
-
-    for epoch in range(100):
+    
+    _p=np.reshape(p*255,(28*class_num,28))
+    total=30
+    for epoch in range(total):
         #E step
         #calculate w
-        '''
-        for c in range(class_num):
-            tmp=(p[c,:]**image_train[:,:])*((1-p[c,:])**(1-image_train[:,:]))
-            print(np.shape(tmp))
-            tmp=np.log(tmp)
-            tmp=np.sum(tmp,axis=1)
-            tmp+=log(_lambda[c])
-
-            w[:,c]=np.exp(tmp)
-        '''    
+    
         tmp=np.dot(image_train,np.log(p.T))+np.dot(1-image_train,np.log(1-p.T))
         tmp[:]+=_lambda
-        w=np.exp(tmp)
-        #for n in range(img_num):
-            #w[n,:]/=(np.sum(w[n,:]))
-       
+        w=np.exp(tmp)  
         w/=(np.sum(w,axis=1).reshape(len(w),1))
        
         
         #M step
        
+    
         _lambda=np.sum(w,axis=0)/img_num
-       
-        #for d in range(dim):
-            #for c in range(class_num):
-                #p[c][d]=max(np.sum(w[:,c]*image_train[:,d])/np.sum(w[:,c]),1e-50)
-        
         
         r=np.dot(w.T,image_train)
         q=np.sum(w,axis=0).reshape(class_num,1)
         p=r/q
         p[p<1e-50]=1e-50
         _lambda[_lambda<1e-5]=1e-5
+
+
+        #epoch log
         print("----------------")
         print("iteration: "+str(epoch))
         print(_lambda)
         error=np.sum(np.abs(pre_lambda-_lambda))
         pre_lambda=_lambda
         print("error: "+str(error))
-        if error <0.005:
+
+        if error <0.0005:
             break
 
+
+        tmp_p=np.reshape(p*255,(28*class_num,28))
+        _p=np.concatenate((_p,tmp_p),axis=1)
         
-  
+        
+        
+    
+    
+
+    img = Image.fromarray(_p)
+    img=img.convert('RGB')
+    img.save('result.png')
+    print(count/img_num)
+
+    classify=np.argmax(w,axis=1)
+    table=np.zeros((class_num,class_num))
+    _table=np.zeros((class_num,class_num))
+
+    '''
+    for i in range(img_num):
+        if classify[i]==label_train[i]:
+            table[label_train[i]][1]+=1
+        else:
+            table[classify[i]][0]+=1
+    '''
+    table=table.astype(int)
+    _table=table.astype(int)
+
+    for i in range(img_num):
+        _table[classify[i]][label_train[i]]+=1
+    
+
+    print(_table)
+    row=input()
+    row=row.split(" ")
+
+    for i in range(len(row)):
+        table[int(row[i])]=_table[i]
+        
+    print(table)
+    print(count)
+
+    for i in range(class_num):
+        print("-----------")
+        print(str(i))
+        TP=table[i][i]
+        FN=count[i]-TP
+        FP=np.sum(table[i])-TP
+        TN=img_num-count[i]-FP
+        sensitivity=TP/(TP+FN)
+        specificity=FP/(FP+TN)
+        print(np.array([[TP,FP],[FN,TN]]))
+        print("sensitivity: "+str(sensitivity))
+        print("specificity: "+str(specificity))
+        
+
+
 if __name__ =="__main__":
     main()
